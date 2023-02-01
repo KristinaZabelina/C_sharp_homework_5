ecc729adbfecceea53545b8d3979a1652b0a7e52
Kristina <zkvzkvzkv@yandex.ru>
2023-02-01 23:24:37 +0300

    создана программа по условию задачи 1

--- hw_5_task_1/Program.cs
+++ hw_5_task_1/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Найдите произведение пар чисел в одномерном массиве. 
+//Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+//Результат запишите в новом массиве.
+
+int[] FillArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(min, max + 1);
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    Console.Write("[" + String.Join(", ", array) + "]");
+}
+
+int[] PairsProd(int[] array)
+{
+    int Len = 0;
+    if (array.Length % 2 == 0) Len = array.Length / 2;
+    else Len = array.Length / 2 + 1;
+    int[] result = new int[Len];
+
+    for (int i = 0; i < Len; i++)
+        result[i] = array[i] * array[array.Length - i - 1];
+
+    if (array.Length % 2 != 0) result[Len - 1] = array[array.Length / 2];
+    return result;
+}
+
+Console.Write("Введите размер массива: ");
+int Size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = FillArray(Size, 1, 5);
+
+ShowArray(array);
+Console.Write($" -> ");
+
+int[] res = PairsProd(array);
+ShowArray(res);
